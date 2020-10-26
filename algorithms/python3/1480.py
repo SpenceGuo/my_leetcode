2e022a957368ac380fed9d9ee15bd2ab5b696584
SpenceGuo <tangguo1280@163.com>
2020-10-26 20:23:43 +0800

    Initial commit

---
+++ algorithms/python3/1480.py
@@ -0,0 +1,14 @@
+class Solution:
+    def runningSum(self, nums):
+        runningSum = []
+        for i in range(0, len(nums)):
+            temp_sum = 0
+            for j in range(0, i+1):
+                temp_sum = temp_sum + nums[j]
+            runningSum.append(temp_sum)
+        return runningSum
+
+
+# testing
+x = Solution()
+print(x.runningSum([1, 2, 3, 4]))
